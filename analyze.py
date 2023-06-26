2f10fa461d2b5229db86c6a3d792d603273d2fd9
Dipokal HHJ <hhj@devent.kr>
2023-06-26 15:52:50 +0900

    feat: record audio

---
+++ analyze.py
@@ -0,0 +1,4 @@
+import librosa
+
+audio_data = 'example.wav'
+x = librosa.load(audio_data, sr=16000)
---
+++ record.py
@@ -0,0 +1,29 @@
+import wave
+import pyaudio
+
+CHUNK = 1024
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=pyaudio.paInt16,
+                channels=2,
+                rate=44100,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+frames = []
+
+for i in range(0, int(44100 / CHUNK * 5)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+
+
+
+stream.close()
+
+wf = wave.open('filename.wav', 'wb')
+wf.setnchannels(1)
+wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
+wf.setframerate(44100)
+wf.writeframes(b''.join(frames))
+p.terminate()
